7b3ea65e7d842fb1b3f3ad0f59b37cbe3f000b1d
Ismail <ismailichu2001@gmail.com>
2026-02-24 11:48:34 +0530

    refactor(auth): error messages change in to const file in redis otp service

--- src/modules/auth/application/constants/error-message.const.ts
+++ src/modules/auth/application/constants/error-message.const.ts
@@ -4,13 +4,16 @@ export const NOT_VERIFIED = 'User not verified';
 export const INVALID_CREDENTIALS = 'Invalid credentials';
 export const ALREADY_REGISTERED = 'User already registered';
 export const ROLE_MISMATCH = 'User role mismatch';
-export const INVALID_OTP = 'Invalid OTP';
 export const OTP_EXPIRED = 'OTP expired';
 export const GOOGLE_LOGIN_INSTEAD = 'Use Google Login Instead';
 export const SIGNUP_AGAIN = 'Signup again';
 
 //otp
 export const OTP_SESSION_EXPIRED = 'Otp Session Expired! Signup again';
+export const INVALID_SESSION = 'Invalid session data';
+export const TOO_MANY_ATTEMPTS = 'Too many attempts, please request a new OTP';
+export const INVALID_OTP_ATTEMPT = 'Invalid OTP, please try again';
+export const WAIT_BEFORE_RESEND = 'Please wait before requesting a new OTP';
 
 //token
 export const SESSION_EXPIRED = 'Session expired do the process again';
